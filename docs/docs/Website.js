36ad1ab5028559e68b21aab48094f5485e60f20a
Sneh Mehta <smehta@colum.edu>
2020-05-04 14:01:14 -0500

    Hopefully we're good here re file structured everything

---
+++ docs/docs/Website.js
@@ -0,0 +1,14 @@
+var insideAbout = false;
+
+$(document).ready(function () {
+    $(".image").click(modalPopout)
+    $(".close").click(close);
+});
+
+function modalPopout() {
+    $(".popout").css("display", "flex");
+}
+
+function close() {
+    $(".popout").css("display", "none");
+}
